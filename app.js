0599e685188337cc453b0e2bf21df38f18125b9b
Dviafara <danielrvt_@hotmail.com>
2025-07-02 23:46:40 -0500

    html-&-css-terminados

---
+++ app.js
@@ -0,0 +1,9 @@
+const contenedor = document.getElementById("contenedor");
+const toppings = document.getElementsByClassName("topping");
+const misToppings = document.getElementsByTagName("li");
+const primerElementoEncontrado = document.querySelector("ul .topping")
+const todosElementosEncontrados = document.querySelectorAll(".topping.fondo-marron")
+console.log(todosElementosEncontrados); 
+// primerElementoEncontrado.style.backgroundColor = "blue";
+// primerElementoEncontrado.style.color = "#6dff00";
+// primerElementoEncontrado.style.textTransform = "uppercase";
